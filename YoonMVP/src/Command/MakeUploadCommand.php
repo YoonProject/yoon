9fb4624c9df795882c96f0fce4dcfcce591878df
yoonisverygood@gmail.com <philip.meeske@outlook.com>
2018-07-08 14:56:51 +0200

    added makeupload command , make upload test, dependency config, WIP

---
+++ YoonMVP/src/Command/MakeUploadCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Yoon\YoonMvp\Command;
+
+use  Yoon\YoonMvp\Command;
+use Rhumsay\Uuid\Uuid;
+
+class MakeUploadCommand implements Command
+{
+
+    function __constructor()
+    {
+
+    }
+
+    /**
+     * Gets the handler id.
+     * @return Rhumsaa\Uuid\Uuid
+     */
+
+    public function getId() : Uuid
+    {
+
+    }
+
+    /**
+     * Gets the message hash signed by the id.
+     * @return string
+     */
+    public function getHashSignedById() : string
+    {
+
+    }
+
+    
+}
+
+?>
